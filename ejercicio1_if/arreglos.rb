797c39412669922a05dcf76c9c0e97b4a9123133
almendra <almendrag@gmail.com>
2021-07-13 11:11:46 -0400

    cambio nombre (operaciones-arreglos)

--- ejercicio1_if/arreglos.rb
+++ ejercicio1_if/arreglos.rb
@@ -1,25 +1,25 @@
-var=[1, 4, 5, 7, 46, 86, 87, 4, 6, 8, 9, 200, 6, 4, 5, 78, 34, 567, 567, 7, 4, 4, 3, 5, 7, 8, 8, 8, 9, 86, 75, 75, 56]
+numeros = [1, 4, 5, 7, 46, 86, 87, 4, 6, 8, 9, 200, 6, 4, 5, 78, 34, 567, 567, 7, 4, 4, 3, 5, 7, 8, 8, 8, 9, 86, 75, 75, 56]
+opcion = "" #lo que el usuarioescriba (suma,resta,pares,etc.)
 
+suma = 0
+resta = 0
+pares = 0
+impares = 0
+mayor = 0
+menor = 0
 
-begin
-  #  puts "Ingrese operacion"
-  #      if suma = gets
-   #         puts 
+opcion = gets.chomp
 
-    #    if resta = gets
-   #         puts
+arreglo_numeros = numeros.split(",")
+puts "Ingrese operacion"
 
-        if pares = gets
-            puts (var%2) == 0
-
-   #     if impares = gets
-    #        puts
-
-    #    if mayor = gets
-    #        puts
-
-    #    if menos = gets
-     #       puts
+if option == "suma"
+     
+elsif opcion == "resta"
+elsif opcion == "pares"
+elsif opcion == "impares"
+elsif opcion == "mayor"
+elsif opcion == "menor"
 
 
 end
@@ -32,4 +32,4 @@ end
 #impares
 #Si el usuario ingresa la palabra "mayor", debe imprimir el número mayor de toda la lista
 #Si el usuario ingresa la palabra "menor", debe imprimir el número menor de toda la
-#lista
+#lista'
